e5aa2aa26849b0760804049d6a7d0068874e67e2
Md. Sakir Ahmed <52326976+engrSakir@users.noreply.github.com>
2024-04-14 16:59:26 +0600

    change response

--- codes/app/Http/Controllers/AttendanceController.php
+++ codes/app/Http/Controllers/AttendanceController.php
@@ -9,12 +9,17 @@ use Rats\Zkteco\Lib\ZKTeco;
 class AttendanceController extends Controller
 {
     public function getData($ip) {
+
         $zk = new ZKTeco($ip);
 
         return response()->json([
             "status" => "Success",
             "connect" => $zk->connect(),
-            "getAttendance" => $zk->getAttendance(),
+            "getAttendance" => json_encode($zk->getAttendance()),
+            "enableDevice" => $zk->enableDevice(),
+            "version" => $zk->version(),
+            "deviceName" => $zk->deviceName(),
+            "getUser" => json_encode($zk->getUser()),
 
         ]);
     }
